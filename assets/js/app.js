e7c538a45344b0da383f9b9b3b361a2c13e91dc7
minhhoccode111 <minhhoccode111@gmail.com>
2022-11-10 18:08:49 +0700

    It's working (kind of...)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,56 @@
 console.log("Hello, World!");
-// interactive UI with JS
 // ******** INITIALIZE VARIABLES **************
+let color = "transparent";
+let colorPen = "black";
+let colorBg = "";
+let draw = true;
+
 const board = document.querySelector("#board");
-const items = board.children;
-console.log(typeof items, items);
-//1. toggle buttons (not contain toggle-grid-btn)
+
+//1. change span of grid size when input is changed
+function createItemsGrid(num) {
+  for (let i = 0; i < num ** 2; i++) {
+    const div = document.createElement("div");
+    div.classList.add("items");
+    div.setAttribute("draggable", "false"); //can't be grabbed
+    div.style.backgroundColor = color;
+    div.addEventListener("mouseover", () => {
+      if (draw) {
+        div.style.backgroundColor = colorPen;
+      } else {
+      }
+    });
+    board.style.cssText = `grid-template-columns: repeat(${num},1fr);grid-template-rows: repeat(${num},1fr)`;
+    board.insertAdjacentElement("beforeend", div);
+  }
+} //this function take a number of grid items then create and append to the parent element #board
+function deleteItemsGrid() {
+  while (board.firstChild) {
+    board.removeChild(board.firstChild);
+  }
+} //this function use to delete old items before new new items pass in
+function sliderInput() {
+  const output = document.querySelector(".show-grid-size");
+  const slider = document.querySelector(".grid-range");
+  output.innerHTML = `Grid Size: ${slider.value}x${slider.value}`;
+  createItemsGrid(slider.value); //use **2 || slider.value*slider.value
+  // setGridTemplate(slider.value);
+  slider.oninput = function () {
+    deleteItemsGrid();
+    createItemsGrid(this.value);
+    //these function create #board 's items
+    output.innerHTML = `Grid Size: ${this.value}x${this.value}`;
+  }; //small span to show grid size
+}
+sliderInput();
+
+//2. toggle buttons (not contain toggle-grid-btn)
 const toggleBtn = document.querySelectorAll(".btn");
-const removeToggle = () => {
+function removeToggle() {
   toggleBtn.forEach((btn) => {
     btn.classList.remove("btn-toggle");
   });
-}; //remove class .btn-toggle from every button
+} //this function remove class .btn-toggle from every button
 toggleBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     if (btn.classList.contains("btn-toggle")) {
@@ -23,56 +63,17 @@ toggleBtn.forEach((btn) => {
     }
   });
 });
-//1.2 toggle grid layout buttons specific to other buttons
-//1.2.1 toggle grid layout to items of #board
-const togGridLayoutItems = () => {
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].nodeName.toLowerCase() == "div") {
-      items[i].classList.toggle("border-tog-items");
-    }
-  }
-};
-// togGridLayoutItems();
-
-//1.2.2 toggle grid layout button
-const gridBorderBtn = document.querySelector("#btn-toggle-border");
-gridBorderBtn.addEventListener("click", () => {
-  gridBorderBtn.classList.toggle("btn-toggle-border"); //this toggle class to button
-  board.classList.toggle("border-tog-ctn"); //this toggle class to show border to #board
-  if (board.classList.contains("border-tog-ctn")) {
-    togGridLayoutItems();
-  }
+//2.2 toggle grid layout buttons specific to other buttons
+//2.2.1 toggle reset button
+const resetBtn = document.querySelector("#btn-reset");
+resetBtn.addEventListener("mouseenter", () => {
+  resetBtn.classList.toggle("btn-toggle");
 });
-//2. change span of grid size when input is changed
-const createItemsGrid = (num) => {
-  if (num == 0) {
-    return;
-  } else {
-    const div = document.createElement("div");
-    board.append(div);
-    return createItemsGrid(num - 1);
-  }
-}; //this function take a number of grid items then create and append to the parent element #board
-const deleteItemsGrid = () => {
-  while (board.firstChild) {
-    board.removeChild(board.firstChild);
-  }
-}; //this function use to delete old items before new new items pass in
-const setGridTemplate = (num) => {
-  board.style.cssText = `grid-template-columns: repeat(${num},1fr);grid-template-rows: repeat(${num},1fr)`;
-}; // this function use to set grid template columns and rows after create items and append to the parent element #board
-const sliderInput = () => {
-  const output = document.querySelector(".show-grid-size");
-  const slider = document.querySelector(".grid-range");
-  output.innerHTML = `Grid Size: ${slider.value}x${slider.value}`;
-  createItemsGrid(slider.value ** 2); //use **2 instead of slider.value *slider.value
-  setGridTemplate(slider.value);
-  slider.oninput = function () {
-    deleteItemsGrid();
-    createItemsGrid(this.value ** 2);
-    setGridTemplate(this.value);
-    output.innerHTML = `Grid Size: ${this.value}x${this.value}`;
-  }; //small span to show grid size
-};
-sliderInput();
-// interactive UI with JS
+resetBtn.addEventListener("mouseleave", () => {
+  resetBtn.classList.toggle("btn-toggle");
+});
+resetBtn.addEventListener("click", () => {
+  sliderInput();
+});
+//################################################################
+// features functions
